d711cc682ff75b847f97bb4f611f6d15f69dece4
Viktar Makouski <viktar@neaar.org>
2024-10-11 08:38:17 +0300

    put acc roots as output

--- program/utreexo/src/main.rs
+++ program/utreexo/src/main.rs
@@ -8,6 +8,7 @@ use rustreexo::accumulator::node_hash::NodeHash;
 use rustreexo::accumulator::pollard::OwnedPollard;
 use rustreexo::accumulator::pollard::Pollard;
 use std::collections::HashMap;
+use std::ops::Deref;
 
 mod btc_structs;
 mod process_block;
@@ -15,11 +16,11 @@ mod process_block;
 use crate::btc_structs::CompactLeafData;
 use crate::process_block::process_block;
 
-sol! {
-    struct PublicValuesStruct {
-        uint32 height;
-    }
-}
+type PublicValuesTuple = sol! {
+    (
+        bytes, // acc roots
+    )
+};
 
 pub fn main() {
     let block = sp1_zkvm::io::read::<Block>();
@@ -29,7 +30,17 @@ pub fn main() {
     let mut acc = Pollard::from_owned_pollard(acc_owned);
     let input_leaf_hashes = sp1_zkvm::io::read::<HashMap<TxIn, (NodeHash, CompactLeafData)>>();
     let (_proof, _compact_leaves) = process_block(&block, height, &mut acc, input_leaf_hashes);
+    let acc_roots: Vec<NodeHash> = acc
+        .get_roots()
+        .to_vec()
+        .iter()
+        .map(|rc| rc.get_data())
+        .collect();
     // TODO: have _proof and _compact_leaves as outputs instead of this
-    let bytes = PublicValuesStruct::abi_encode(&PublicValuesStruct { height });
+    let acc_roots_bytes: Vec<[u8; 32]> = acc_roots.iter().map(|hash| *hash.deref()).collect();
+    let acc_roots_bytes_flat: Vec<u8> = acc_roots_bytes.concat();
+
+    let bytes = PublicValuesTuple::abi_encode(&(acc_roots_bytes_flat,));
+
     sp1_zkvm::io::commit_slice(&bytes);
 }
