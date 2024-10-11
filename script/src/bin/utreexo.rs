f2a87184259a5b5c2355fae413cbd005d91aedae
Viktar Makouski <viktar@neaar.org>
2024-10-11 12:16:53 +0300

    First proving version

--- script/src/bin/utreexo.rs
+++ script/src/bin/utreexo.rs
@@ -1,13 +1,29 @@
 use bitcoin::TxIn;
 use bitcoin::{block, Block};
+use clap::Parser;
 use reqwest;
 use rustreexo::accumulator::node_hash::NodeHash;
 use rustreexo::accumulator::pollard::{OwnedPollard, Pollard};
 use serde::{Deserialize, Serialize};
+use sp1_sdk::network::client;
 use sp1_sdk::{utils, ProverClient, SP1Stdin};
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
+
+/// The arguments for the command.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(long)]
+    execute: bool,
+
+    #[clap(long)]
+    prove: bool,
+
+    #[clap(long, default_value = "20")]
+    n: u32,
+}
 
 #[derive(PartialEq, Eq, Clone, Debug, Serialize, Deserialize)]
 pub struct CompactLeafData {
@@ -57,6 +73,14 @@ async fn get_block(height: u32) -> Result<Block, Box<dyn Error>> {
 async fn main() -> Result<(), Box<dyn Error>> {
     utils::setup_logger();
 
+    // Parse the command line arguments.
+    let args = Args::parse();
+
+    if args.execute == args.prove {
+        eprintln!("Error: You must specify either --execute or --prove");
+        std::process::exit(1);
+    }
+
     let height = 1;
     let acc_file = File::open("../acc-data/acc-before-1.txt")?;
     // let acc = Pollard::deserialize(reader).unwrap();
@@ -72,10 +96,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
     stdin.write::<OwnedPollard>(&owned_acc);
     stdin.write::<HashMap<TxIn, (NodeHash, CompactLeafData)>>(&input_leaf_hashes);
 
-    let client = ProverClient::new();
-    let public_values = client.execute(ELF, stdin).run().unwrap();
-
-    println!("proof generated");
-    // println!("output: {:#?}", public_values);
+    if args.execute {
+        let client = ProverClient::new();
+        let public_values = client.execute(ELF, stdin).run().unwrap();
+        println!("Succesfully executed");
+        println!("output: {:#?}", public_values);
+    } else {
+        let client = ProverClient::new();
+        let (pk, vk) = client.setup(ELF);
+        let proof = client
+            .prove(&pk, stdin)
+            .run()
+            .expect("failed to generate proof");
+        println!("Successfully generated proof!");
+        client.verify(&proof, &vk).expect("failed to verify proof");
+        println!("Successfully verified proof!");
+    }
     Ok(())
 }
